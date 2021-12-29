5684a1657c2302fe33148570e36df740e5332625
MarlonDValencia <marlondvalencia@gmail.com>
2021-12-29 16:20:00 -0500

    Project initialized

---
+++ libreriaSpringMongo/src/main/java/com/libreria/libreriaSpringMongo/LibreriaSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.libreria.libreriaSpringMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaSpringMongoApplication.class, args);
+	}
+
+}
---
+++ libreriaSpringMongo/src/test/java/com/libreria/libreriaSpringMongo/LibreriaSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libreria.libreriaSpringMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
